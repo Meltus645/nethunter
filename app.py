ac52dca2c53cd3d69222541037699f74c5b89b8c
=wajuma <=meltusjuma5@gmail.com>
2021-07-11 21:04:21 +0300

    update rename

--- app.py
+++ app.py
@@ -4,18 +4,17 @@ class RenameFiles:
     default_dir ='/Users/wajuma/Downloads/Video'
     def __init__(self, path =default_dir):
         self.path =path
-        self.rename(self.check_keyword)
-    def rename(self, filename):
+        self.rename()
+    def rename(self):
         os.chdir(self.path)
         for f in os.listdir():
             f_name,f_ext =os.path.splitext(f)
-            n_name =f_name.lower().split('y2mate.com')[1].strip()
-            if n_name[0] =='-':
-                n_name =f_name.split('-')[1].strip()
-            n_name =n_name+f_ext
-            print(n_name.find('y2mate'))
-    def check_keyword(self):
-        return filename
-        
+            n_name =f_name.lower().strip()
+            kwindex =n_name.find('y2mate.com');
+            if kwindex >=0:
+                tail =f_name[:kwindex]
+                head =f_name[kwindex+len('y2mate.com'):]
+                print(tail+head)
+
 if __name__ =='__main__':
     RenameFiles()
