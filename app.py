b4c34822721f75513a77759a8d7dba2f11f83a83
=wajuma <=meltusjuma5@gmail.com>
2021-07-11 19:48:11 +0300

    split file name and extension

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
 
 class RenameFiles:
-    default_dir ='/Users/wajuma/Downloads'
+    default_dir ='/Users/wajuma/Downloads/Video'
     def __init__(self, path =default_dir):
         self.path =path
         self.rename()
     def rename(self):
         os.chdir(self.path)
-        print(os.getcwd())
+        for file in os.listdir():
+            name,extension =os.path.splitext(file)
+            print(name)
 
 if __name__ =='__main__':
     RenameFiles()
