bf12f7dbdc74c0e32bcbd72c95f6f5395c066a99
=wajuma <=meltusjuma5@gmail.com>
2021-07-11 20:40:45 +0300

    check keywords

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ class RenameFiles:
     default_dir ='/Users/wajuma/Downloads/Video'
     def __init__(self, path =default_dir):
         self.path =path
-        self.rename()
-    def rename(self):
+        self.rename(self.check_keyword)
+    def rename(self, filename):
         os.chdir(self.path)
         for f in os.listdir():
             f_name,f_ext =os.path.splitext(f)
@@ -13,7 +13,9 @@ class RenameFiles:
             if n_name[0] =='-':
                 n_name =f_name.split('-')[1].strip()
             n_name =n_name+f_ext
-            print(n_name)
-
+            print(n_name.find('y2mate'))
+    def check_keyword(self):
+        return filename
+        
 if __name__ =='__main__':
     RenameFiles()
