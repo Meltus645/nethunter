8d5f827aff90d49d90741437e0c21e557ae61a5d
=wajuma <=meltusjuma5@gmail.com>
2021-07-12 01:04:41 +0300

    strip hyphen

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ class RenameFiles:
             kwindex =f_name.lower().find('y2mate.com');
             n_name =''
             if kwindex >=0:
-                tail =f_name[:kwindex].lstrip()
-                head =f_name[kwindex+len('y2mate.com'):].rstrip()
+                tail =f_name[:kwindex].strip()
+                head =f_name[kwindex+len('y2mate.com'):].strip()
                 n_name =tail+head
                 if n_name[0] =='-':
-                    
-            print(n_name)
+                    n_name =n_name[1:].strip()
+                print(n_name+f_ext)
 
 if __name__ =='__main__':
     RenameFiles()
